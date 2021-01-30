ad5720539c42a6b8c0ec2eaf0069f213f4f821cb
najibkado <dopeland3@gmail.com>
2021-01-30 12:49:40 +0400

    User id int

--- main/views.py
+++ main/views.py
@@ -80,7 +80,7 @@ class UserRegisterApiView(APIView):
             serializer.save()
             user = User.objects.get(username=serializer.data['username'])
             token = Token.objects.create(user=user)
-            return Response({'data': serializer.data, 'token': token.key }, status=status.HTTP_201_CREATED)
+            return Response({'user_id': user.id, 'data': serializer.data, 'token': token.key }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
